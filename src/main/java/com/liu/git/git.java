5ea0b3a693704e102163e91fff034983a564ef8b
liu <1574717571@qq.com>
2023-10-31 14:33:27 +0800

    tree commit

---
+++ src/main/java/com/liu/git/git.java
@@ -0,0 +1,9 @@
+package com.liu.git;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
